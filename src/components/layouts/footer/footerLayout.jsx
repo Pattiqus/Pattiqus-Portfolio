1c358720ca60cfd5a9338fb9e553d76a91a89aae
unknown <patticus.tv@gmail.com>
2022-04-20 12:10:21 +1000

    footer positioning complete, without style

--- src/components/layouts/footer/footerLayout.jsx
+++ src/components/layouts/footer/footerLayout.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Link } from "react-router-dom";
-import { Icon } from '@fortawesome/fontawesome-svg-core'; 
-import { faIcons } from '@fortawesome/free-solid-svg-icons';
-
+import { faGithub, faLinkedin, faStackOverflow, faTwitter } from '@fortawesome/free-brands-svg-icons'
+import './footerLayout.css';
 
 
 export default function footerLayout(props) {
   
     const socialLinks = [
         {
-            title: <FontAwesomeIcon Icon="fa-brands fa-github" />,
+            title: <FontAwesomeIcon icon={faGithub} />,
             link: 'https://github.com/Pattiqus'
         },
         {
-            title: <FontAwesomeIcon icon="fa-brands fa-linkedin-in"/>,
+            title: <FontAwesomeIcon icon={faLinkedin}/>,
             link: 'https://www.linkedin.com/in/pat-b-52553410a/'
         },
         {
-            title: <FontAwesomeIcon icon="fa-brands fa-twitter" />,
+            title: <FontAwesomeIcon icon={faTwitter} />,
             link: 'https://twitter.com/Patticus_TV'
         },
         {
-            title: <FontAwesomeIcon icon="fa-brands fa-stack-overflow" />,
+            title: <FontAwesomeIcon icon={faStackOverflow} />,
             link: 'https://stackoverflow.com/users/17328724/patticus'
         },
     ]
@@ -32,9 +31,9 @@ export default function footerLayout(props) {
 
         {/*Footer*/}
         
-        <ul>
+        <ul className='footerLinksList'>
         {socialLinks.map((item) => (
-                <li className=''>
+                <li className='footerButtons'>
                     <Link to={item.link}>
                        {item.title}
                     </Link>
