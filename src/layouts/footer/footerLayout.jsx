333604896262a6931f405c405d023dc0a48d76cf
unknown <patticus.tv@gmail.com>
2022-04-28 22:02:05 +1000

    removed link in footer not needed

--- src/layouts/footer/footerLayout.jsx
+++ src/layouts/footer/footerLayout.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from "react-router-dom";
 import { faGithub, faLinkedin, faStackOverflow, faTwitter } from '@fortawesome/free-brands-svg-icons'
 import styled from 'styled-components';
 
