beccf5359a229173593c4de0ce76550c9a2400b7
unknown <patticus.tv@gmail.com>
2022-04-29 16:59:02 +1000

    adding projects to cards

--- src/assets/data/Projects.jsx
+++ src/assets/data/Projects.jsx
@@ -19,24 +19,36 @@ const projects = [
   },
   {
     id: uuidv4(),
-    name: 'Lorem',
+    name: 'Dynamic Express Based Note Taker',
     desc:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'A note taking application which utilises express.js & Typescript. Perfect for a to do or shopping list',
     img: placeholderImg,
+    hyperGit: <a href='https://github.com/Pattiqus/dynamic-express-based-note-taker'>
+    <FontAwesomeIcon icon={faGithub}/></a>,
+    hyperDeployed: <a href='https://note-taker-safe-temple-09707.herokuapp.com/'>
+    <div className='icon'>{<IoRocket/>}</div></a>
   },
   {
     id: uuidv4(),
-    name: 'Lorem',
+    name: 'myRecipe',
     desc:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'myRecipe is an application in which users have the ability to post their favourite recipes online for peers and other culinary enthusiasts around the world.',
     img: placeholderImg,
+    hyperGit: <a href='https://github.com/Pattiqus/myRecipe'>
+    <FontAwesomeIcon icon={faGithub}/></a>,
+    hyperDeployed: <a href='https://my-recipe-pbmm.herokuapp.com/'>
+    <div className='icon'>{<IoRocket/>}</div></a>
   },
   {
     id: uuidv4(),
-    name: "Lorem",
+    name: "Traveler's Weather Dashboard",
     desc:
-      'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      'An application to get the weather from current location, or anywhere in the world, at anytime',
     img: placeholderImg,
+    hyperGit: <a href='https://github.com/Pattiqus/6Week-6-Weather-Dashboard'>
+    <FontAwesomeIcon icon={faGithub}/></a>,
+    hyperDeployed: <a href='https://my-recipe-pbmm.herokuapp.com/'>
+    <div className='icon'>{<IoRocket/>}</div></a>
   },
   {
     id: uuidv4(),
